41bddca5e2f4d2ad7409c0c180b1952b035add1c
zosyk <alekcandrzosyk@gmail.com>
2017-04-19 13:33:15 +0300

    copy function

---
+++ src/main/java/gd/test/Project1Application.java
@@ -0,0 +1,12 @@
+package gd.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project1Application.class, args);
+	}
+}
---
+++ src/main/resources/public/js/main.js
@@ -0,0 +1,25 @@
+var ngApp = angular.module('angularDemo', [])
+    .controller("AppCtrl", function ($scope) {
+
+        $scope.selectedUser = null;
+        $scope.userCopy = null;
+
+       $scope.users = [
+           {firstName: 'Mark', lastName: 'Otto', username: '@mdo'},
+           {firstName: 'Jacob', lastName: 'Thornton', username: '@fat'},
+           {firstName: 'Larry', lastName: 'the Bird', username: '@twitter'}
+       ];
+
+       $scope.selectUser = function (user) {
+           $scope.selectedUser = user;
+
+           $scope.userCopy = angular.copy(user);
+       }
+
+       $scope.saveUser = function (user) {
+           $scope.selectedUser.firstName = user.firstName;
+           $scope.userCopy = null;
+       }
+
+    });
+
